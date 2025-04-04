f53fa89dd1a0854e561fa6358f7e55ef82078373
unknown <jun9048@naver.com>
2025-04-04 16:50:01 +0900

    feat: typing issues2

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
 declare global {
